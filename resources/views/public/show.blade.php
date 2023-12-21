a12178306ca2493d8461bb98a0291fbd7d359838
tombury <tombury59@hotmail.com>
2023-12-21 15:45:24 +0100

    ajout des commentaires

--- resources/views/public/show.blade.php
+++ resources/views/public/show.blade.php
@@ -14,4 +14,32 @@
             <p class="text-gray-700 dark:text-gray-300">{{ $article->content }}</p>
         </div>
     </div>
+
+
+    @auth
+        <div class="shadow-lg p-6 bg-gray-200 rounded">
+            @foreach ($article->comments as $comment)
+                <div class="bg-gray-100 p-4 my-4 rounded-md">
+                    <p class="text-gray-700 text-sm">{{ $comment->content }}</p>
+                    <div class="mt-2 flex items-center">
+                        <span class="text-gray-500 text-xs">{{ $comment->user->name }}</span>
+                        <span class="text-gray-500 text-xs ml-2">{{ $comment->created_at->diffForHumans() }}</span>
+                    </div>
+                </div>
+            @endforeach
+
+            <form action="{{ route('comments.store', ['article' => $article->id]) }}" method="post" class="mt-6">
+                @csrf
+                <input type="hidden" name="article" value="{{ $article->id }}">
+
+                <div class="mb-4">
+                    <label for="content" class="block text-gray-700 text-sm font-bold mb-2">Ajouter un commentaire:</label>
+                    <textarea name="content" id="content" rows="3" class="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"></textarea>
+                </div>
+
+                <button type="submit" class="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600">Envoyer</button>
+            </form>
+        </div>
+    @endauth
+
 </x-guest-layout>
